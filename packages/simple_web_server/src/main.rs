02ad20af453826b78dddfd04817d60a7cb488b9e
actake <960550591@qq.com>
2023-04-18 21:55:40 +0800

    fix: 处理在不同目录启动时资源引用报错的问题

--- packages/simple_web_server/src/main.rs
+++ packages/simple_web_server/src/main.rs
@@ -1,7 +1,8 @@
 use std::{
-    fs,
+    env, fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    path::PathBuf,
 };
 
 fn main() {
@@ -17,13 +18,17 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     // println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+    let resource_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("src")
+        .join("resources");
 
     let (status_line, filename) = if buffer.starts_with(b"GET / HTTP/1.1") {
-        ("HTTP/1.1 200 OK", "resources/hello.html")
+        ("HTTP/1.1 200 OK", resource_path.join("hello.html"))
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "resources/404.html")
+        ("HTTP/1.1 404 NOT FOUND", resource_path.join("404.html"))
     };
 
+    println!("filename: {:?}", filename);
     let html_content = fs::read_to_string(filename).unwrap();
     let res = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
